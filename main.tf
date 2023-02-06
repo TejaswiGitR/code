b972fa6620a1d021456a4c515be3df6d433b9f77
TejaswiGitR <123970489+TejaswiGitR@users.noreply.github.com>
2023-02-06 22:03:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,11 +21,3 @@ resource "azurerm_resource_group" "Rg2" {
 }
 
 
-
-resource "azurerm_virtual_network" "VN2" {
-  name                = "prefix"
-  location            = "azurerm_resource_group.Rg2.location"
-  resource_group_name = "azurerm_resource_group.Rg2.name"
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
-  }
